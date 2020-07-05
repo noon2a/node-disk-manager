21321f6b1bf0eda82f47a4e9cb8d071cea0aa6db
Akhil Mohan <akhil.mohan@mayadata.io>
2020-07-05 23:57:51 +0530

    remove kubectl
    Signed-off-by: Akhil Mohan <akhil.mohan@mayadata.io>

--- minikube.sh
+++ minikube.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-echo "Downloading Kubectl Binary..."
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
-echo "Kubectl Client Version..."
-kubectl version --client
-
 echo "Downloading Minikube Binary..."
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 \
   && chmod +x minikube
